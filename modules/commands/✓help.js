ab40c65f3c820080a6038728669f91e24c2b3ca6
atshyqwkwrsaky-create <atshyqwkwrsaky@gmail.com>
2026-05-01 17:39:36 +0200

    Update ✓help.js

--- modules/commands/✓help.js
+++ modules/commands/✓help.js
@@ -1,4 +1,3 @@
-
 module.exports.config = {
   name: "اوامر",
   version: "1.0.2",
@@ -19,8 +18,8 @@ module.exports.languages = {
     "moduleInfo": "「 %1 」\n%2\n\n❯ Usage: %3\n❯ Category: %4\n❯ Waiting time: %5 seconds(s)\n❯ Permission: %6\n\n» Module code by %7 «",
     "helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! ]',
     "user": "User",
-        "adminGroup": "Admin group",
-        "adminBot": "Admin bot"
+    "adminGroup": "Admin group",
+    "adminBot": "Admin bot"
   }
 };
 
@@ -31,13 +30,28 @@ module.exports.handleEvent = function ({ api, event, getText }) {
   if (!body || typeof body == "cmd" || body.indexOf("help") != 0) return;
   const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
   if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
+
   const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
   const command = commands.get(splitBody[1].toLowerCase());
   const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
-  return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
-}
 
-module.exports. run = function({ api, event, args, getText }) {
+  return api.sendMessage(
+    getText(
+      "moduleInfo",
+      command.config.name,
+      command.config.description,
+      `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`,
+      command.config.commandCategory,
+      command.config.cooldowns,
+      ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")),
+      command.config.credits
+    ),
+    threadID,
+    messageID
+  );
+};
+
+module.exports.run = function({ api, event, args, getText }) {
   const { commands } = global.client;
   const { threadID, messageID } = event;
   const command = commands.get((args[0] || "").toLowerCase());
@@ -50,22 +64,33 @@ module.exports. run = function({ api, event, args, getText }) {
     const page = parseInt(args[0]) || 1;
     const numberOfOnePage = 100;
     let i = 0;
-    let msg = "-·=»〖〗«=·-\n🍃 اوامر بوت ᏴϴᏆ. ぐ愛 🍃\n-·=»〖〗«=·-\n✨🍃\n";
 
-    for (var [name, value] of (commands)) {
+    for (var [name, value] of commands) {
       arrayInfo.push(name);
     }
 
-    arrayInfo.sort((a, b) => a.data - b.data);
+    arrayInfo.sort((a, b) => a.localeCompare(b));
 
-    const startSlice = numberOfOnePage*page - numberOfOnePage;
+    const startSlice = numberOfOnePage * page - numberOfOnePage;
     i = startSlice;
     const returnArray = arrayInfo.slice(startSlice, startSlice + numberOfOnePage);
 
-    for (let item of returnArray) msg += ` 〖💀🍃〗  ${++i} . 『${item}』\n\n\n${commands.get(item).config.description}\n—͟͟͞͞��\n`;
-    const randomText = [ "hy bhy baby","g","h"];
-    const text = `🍃—͟͟͞͞��\n-·=»〖〗«=·-\n🍃   صفحة واحد (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}) 🍃\n-·=»〖〗«=·-\nBOT: °${prefix} ᏴϴᏆ. ぐ愛°\nقائمة📜 الاوامر: ${arrayInfo.length} `;
-    return api.sendMessage(msg  + text, threadID, async (error, info) => {
+    // ✨ الشكل الجديد
+    let msg = `───═━━━━━
+⏤͟͟͞͞   𝙲𝙼𝙳    𝙻𝙸𝚂𝚃
+────────
+`;
+
+    for (let item of returnArray) {
+      msg += `⑉ ${item}\n`;
+    }
+
+    let text = `────────
+.𝙰𝙻𝙻 𝙲𝙼𝙳 : ${arrayInfo.length}
+.𝙿𝙰𝙶𝙴 ${page} 𝙾𝙵 ${Math.ceil(arrayInfo.length / numberOfOnePage)}
+────────`;
+
+    return api.sendMessage(msg + text, threadID, async (error, info) => {
       if (autoUnsend) {
         await new Promise(resolve => setTimeout(resolve, delayUnsend * 10000000));
         return api.unsendMessage(info.messageID);
@@ -73,5 +98,18 @@ module.exports. run = function({ api, event, args, getText }) {
     });
   }
 
-  return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
+  return api.sendMessage(
+    getText(
+      "moduleInfo",
+      command.config.name,
+      command.config.description,
+      `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`,
+      command.config.commandCategory,
+      command.config.cooldowns,
+      ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")),
+      command.config.credits
+    ),
+    threadID,
+    messageID
+  );
 };
